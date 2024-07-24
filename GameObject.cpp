b3883084b09ee9c654260aa8655568a2fa3e8c04
FoxThings <nik.zve54321@gmail.com>
2024-07-25 00:51:36 +0300

    Render improvements

--- GameObject.cpp
+++ GameObject.cpp
@@ -15,15 +15,18 @@ void GameObject::SetPosition(Vector2D newPosition)
 void GameObject::Render(uint32_t* buffer, uint32_t width, uint32_t height)
 {
 	Vector2D size = this->shape.size;
-	Vector2D position = this->position;
+
+	// Center pivot coordinate system
+	Vector2D position = this->position - size * 0.5;
 	
-	for (uint32_t y = (uint32_t)position.y; y < position.y + size.y; ++y) {
-		for (uint32_t x = (uint32_t)position.x; x < position.x + size.x; ++x) {
+	// left-down corned coordinate system
+	for (int y = height - (int)position.y - size.y; y < height - position.y; ++y) {
+		for (int x = (int)position.x; x < position.x + size.x; ++x) {
 			if (
-				(x > 0 && x < width) && 
-				(y > 0 && y < height)) 
+				(x >= 0 && x < width) && 
+				(y >= 0 && y < height))
 			{
-				buffer[x * width + y] = 4515146844685;
+				buffer[y * width + x] = 4515146844685;
 			}
 		}
 	}
