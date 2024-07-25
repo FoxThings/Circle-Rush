4b2b76e9ffb2887fba2b198d9307ac2810a2f715
FoxThings <nik.zve54321@gmail.com>
2024-07-25 23:53:32 +0300

    Physics fixes

--- Simulation.cpp
+++ Simulation.cpp
@@ -6,8 +6,8 @@ void Simulation::Update()
 	std::vector<std::function<void()>> procedures = std::vector<std::function<void()>>();
 
 	// So stupid physic simulation :)
-	for (size_t i = 0; i < objects->size(); ++i) {
-		for (size_t j = i + 1; j < objects->size() - 1; ++j) {
+	for (size_t i = 0; i < objects->size() - 1; ++i) {
+		for (size_t j = i + 1; j < objects->size(); ++j) {
 			GameObject* first = objects->at(i);
 			GameObject* second = objects->at(j);
 
@@ -16,14 +16,16 @@ void Simulation::Update()
 
 			// Center pivot coordinate system to UpLeftCorner
 			Vector2D firstPostion = first->position - firstSize * 0.5;
-			Vector2D secondsPostion = second->position - secondSize * 0.5;
+			Vector2D secondPostion = second->position - secondSize * 0.5;
 
 			if (
-				firstPostion.x < secondsPostion.x + secondSize.x &&
-				firstPostion.x + firstSize.x > secondsPostion.x &&
-				firstPostion.y < secondsPostion.y + secondSize.y &&
-				firstPostion.y + firstSize.y > secondsPostion.y
+				firstPostion.x < secondPostion.x + secondSize.x &&
+				firstPostion.x + firstSize.x > secondPostion.x &&
+				firstPostion.y + firstSize.y > secondPostion.y &&
+				firstPostion.y < secondPostion.y + secondSize.y
 				) {
+				first->color = 214715;
+				second->color = 214715;
 				procedures.push_back(
 					[first, second]() {
 					if (first->collider.callback != nullptr) {
